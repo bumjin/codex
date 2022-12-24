d94a5d53b0bae469d55dab408082cf25b1cd6227
Beomjin Park <bumjin@gmail.com>
2022-12-24 11:44:53 +0900

    update the server url

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from serve -> bot's response
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-3k7t.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
